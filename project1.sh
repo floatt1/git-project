dbbb48c81675d4311a358317f500eb25b6524ea0
float1 <float1860@gmail.com>
2023-06-15 01:43:12 +0000

    changes made to file

--- project1.sh
+++ project1.sh
@@ -1,2 +1,2 @@
 This is my world and i will make it a better place for everyone around me 
-beause it is one world and it is all we got. peace 
+beause it is one world and it is all we got. We will maintain the sanity in this world. 
